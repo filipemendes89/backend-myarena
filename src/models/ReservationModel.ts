0ee6284f1386ecd48d391fec5ea0e1be397f2673
filipe.mendes <filipe.mendes@totvs.com.br>
2023-09-29 18:58:29 -0300

    fix: model reservation

--- src/models/ReservationModel.ts
+++ src/models/ReservationModel.ts
@@ -4,10 +4,10 @@ const reservationSchema = new mongoose.Schema({
   createdAt: { type: Date, required: true, default: new Date() },
   courtId: { type: String, required: true },
   time: { type: String, required: true },
-  classId: { type: String, required: true },
+  classId: { type: String, required: false },
   date: { type: String, required: true },
   reserverId: { type: String, required: false },
-  active: { type: Boolean, required: true },
+  active: { type: Boolean, required: true, default: true },
   tenantId: { type: String, default: "1" }
 });
   
