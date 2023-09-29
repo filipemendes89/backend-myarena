bc3d49fccdb4f0bb736f921ab243e4d874f22c2b
filipe.mendes <filipe.mendes@totvs.com.br>
2023-09-29 20:08:35 -0300

    fix: validacao da reserva por pessoa ou aula

--- reservation/services/handlePost.ts
+++ reservation/services/handlePost.ts
@@ -2,7 +2,14 @@ import { HttpRequest } from "@azure/functions"
 import { Context } from "vm"
 import { Reservation } from "../../src/models/ReservationModel"
 
-export default async (context: Context,req: HttpRequest) => context.res = {
-    body: await new Reservation(req.body).save(),
-    status: 200
+export default async (context: Context,req: HttpRequest) => {
+    const { reserverId, classId } = req.body
+
+    if(!reserverId && !classId)
+        throw new Error('ReserverId or ClassId are required')
+        
+    return context.res = {
+        body: await new Reservation(req.body).save(),
+        status: 200
+    }
 }
