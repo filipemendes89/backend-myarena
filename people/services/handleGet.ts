aad7075afb3f17d5be789afa09072d39182b36bc
filipe.mendes <filipe.mendes@totvs.com.br>
2023-10-02 11:05:40 -0300

    feat: adicionado filtro para pessoas

--- people/services/handleGet.ts
+++ people/services/handleGet.ts
@@ -4,7 +4,7 @@ import { Context } from "vm"
 import { People } from "../../src/models/PeopleModel"
 
 export default async (context: Context,req: HttpRequest) => { 
-    const { page, pageSize, email } = req.query
+    const { page, pageSize, email, filter } = req.query
     const id = req.params.id
 
     const modelPeople = model('People').schema.obj
@@ -14,6 +14,9 @@ export default async (context: Context,req: HttpRequest) => {
             delete req.query[key]
         }
     })
+
+    if(filter)
+        return getFilteredPeople(context,req, filter, page, pageSize)
 
     if(id)
         return context.res = { body: await People.findById(id).exec() }
@@ -31,6 +34,29 @@ export default async (context: Context,req: HttpRequest) => {
         body: { 
             items : people, 
             hasNext: Math.ceil(totalPeople / Number(pageSize)) > Number(page) 
+        },
+        status: 200
+    }
+}
+
+const getFilteredPeople = async (context: Context,req: HttpRequest, filter: string, page: string, pageSize: string) => {
+    const totalFiltered = await People.find({ $or: [
+        { cpf: { $regex: filter, $options: 'i' } },
+        { nome: { $regex: filter, $options: 'i' } }
+      ]}).count()
+
+    const filteredPeople = await People.find({ $or: [
+        { cpf: { $regex: filter, $options: 'i' } },
+        { nome: { $regex: filter, $options: 'i' } }
+    ]})
+    .limit(Number(pageSize) * 1)
+    .skip((Number(page) - 1) * Number(pageSize))
+    .exec()
+
+    return context.res = {
+        body: { 
+            items : filteredPeople, 
+            hasNext: Math.ceil(totalFiltered / Number(pageSize)) > Number(page) 
         },
         status: 200
     }
