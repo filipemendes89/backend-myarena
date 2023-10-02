3c1581893b2c2a6a874288aa85c9de95dbfaec7c
filipe.mendes <filipe.mendes@totvs.com.br>
2023-10-02 17:18:40 -0300

    fix: correcao da tag isfull

--- class/services/handlePut.ts
+++ class/services/handlePut.ts
@@ -3,7 +3,7 @@ import { Context } from "vm"
 import { Class } from "../../src/models/ClassModel"
 
 export default async (context: Context,req: HttpRequest) => { 
-    req.body.isIsFull = req.body.peopleList?.length > req.body.people 
+    req.body.isIsFull = req.body.peopleList?.length >= req.body.people 
     
     return context.res = {
         body: await Class.findByIdAndUpdate(req.params.id, req.body, { new: true }),
