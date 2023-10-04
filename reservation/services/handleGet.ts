d373d9b4584070155a6b94a94cdd3eb7bfbd5851
filipe.mendes <filipe.mendes@totvs.com.br>
2023-10-04 17:40:30 -0300

    feat: inner populate da reserva

--- reservation/services/handleGet.ts
+++ reservation/services/handleGet.ts
@@ -16,13 +16,22 @@ export default async (context: Context,req: HttpRequest) => {
     })
 
     if(id)
-        return context.res = { body: await Reservation.findById(id).populate('reserverId').exec() }
+        return context.res = { body: await Reservation.findById(id).populate('reserverId').populate('courtId')
+        .populate('classId').exec() }
         
     const totalReservation = await Reservation.find(req.query).count()
     const classFound = await Reservation.find(req.query)
     .limit(Number(pageSize) * 1)
     .skip((Number(page) - 1) * Number(pageSize))
     .populate('reserverId')
+    .populate('courtId')
+    .populate('classId')
+    .populate({
+        path: 'classId',
+        populate: {
+            path: 'teacherId'
+        }
+    })
     .exec()
 
     return context.res = {
