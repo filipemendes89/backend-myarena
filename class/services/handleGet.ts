24ca3802c3d6b8ad713c9de9456ecbd8b89fcd9a
filipe.mendes <filipe.mendes@totvs.com.br>
2023-10-04 12:42:35 -0300

    feat: busca por id no array de alunos

--- class/services/handleGet.ts
+++ class/services/handleGet.ts
@@ -1,10 +1,10 @@
 import { HttpRequest } from "@azure/functions"
-import { model } from "mongoose"
+import mongoose, { model } from "mongoose"
 import { Context } from "vm"
 import { Class } from "../../src/models/ClassModel"
 
 export default async (context: Context,req: HttpRequest) => { 
-    const { page, pageSize } = req.query
+    const { page, pageSize, memberId } = req.query
     const id = req.params.id
 
     const modelClass = model('Class').schema.obj
@@ -17,6 +17,9 @@ export default async (context: Context,req: HttpRequest) => {
 
     if(id)
         return context.res = { body: await Class.findById(id).exec() }
+    
+    if(memberId)
+        return await handleMemberId(context, req, memberId, page, pageSize)
         
     const totalClass = await Class.find(req.query).count()
     const classFound = await Class.find(req.query)
@@ -28,6 +31,26 @@ export default async (context: Context,req: HttpRequest) => {
         body: { 
             items : classFound, 
             hasNext: Math.ceil(totalClass / Number(pageSize)) > Number(page) 
+        },
+        status: 200
+    }
+}
+
+const handleMemberId = async (context: Context, req: HttpRequest, memberId: string, page: string, pageSize: string) => {
+    const query = {
+        "peopleList._id": new mongoose.Types.ObjectId(memberId)
+    }
+    
+    const totalClasses = await Class.find(query).count()
+    const classesFound = await Class.find(query)
+    .limit(Number(pageSize) * 1)
+    .skip((Number(page) - 1) * Number(pageSize))
+    .exec()
+
+    return context.res = {
+        body: { 
+            items : classesFound, 
+            hasNext: Math.ceil(totalClasses / Number(pageSize)) > Number(page) 
         },
         status: 200
     }
